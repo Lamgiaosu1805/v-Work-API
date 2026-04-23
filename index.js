34a4db0975bc6f7ef128131a1f2a15216b2e6d37
Lâm GS <nghiemkhaclam185.mta@gmail.com>
2026-04-23 14:23:57 +0700

    fix cors

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ process.env.TZ = 'Asia/Ho_Chi_Minh';
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const path = require('path'); // thêm dòng này
+const cors = require('cors');
+const path = require('path');
 const db = require('./src/config/connectDB');
 const route = require('./src/routes');
 
 const app = express();
 
 // Middlewares
+app.use(cors()); // Mở hoàn toàn
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // sửa dòng này
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Request & Response logging middleware
 app.use(require('./src/middlewares/loggingMiddleware'));

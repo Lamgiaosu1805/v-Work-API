1ef603356252054f00ac5da49ccbb22a37e46fec
trungnv2010 <nguyenvutrung2010@gmail.com>
2026-08-24 08:07:16 +0700

    feat: auto reject

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -3,7 +3,7 @@ const { registerFinalizeWorkDayJob } = require("./finalizeWorkDay");
 const { registerCleanupDeviceTokensJob } = require("./cleanupDeviceTokens");
 const { registerWeeklyReportJobs } = require("./weeklyReportJob");
 const { registerAccrueMonthlyLeaveJob } = require("./accrueMonthlyLeave");
-const { registerAutoRejectLeaveRequestsJob } = require("./autoRejectLeaveRequests");
+// const { registerAutoRejectLeaveRequestsJob } = require("./autoRejectLeaveRequests");
 const { registerChurnDetectionJob } = require("./churnDetectionJob");
 const { registerSharedFolderCleanupJob } = require("./sharedFolderCleanupJob");
 
@@ -13,7 +13,7 @@ function startCronJobs() {
   registerCleanupDeviceTokensJob();
   registerWeeklyReportJobs();
   registerAccrueMonthlyLeaveJob();
-  registerAutoRejectLeaveRequestsJob();
+  // registerAutoRejectLeaveRequestsJob();
   registerChurnDetectionJob();
   registerSharedFolderCleanupJob();
 }
